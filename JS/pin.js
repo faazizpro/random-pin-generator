f75c0d9c45fdb5fb94d471fc08e62f9a096a5912
faazizpro <fahadafs11@gmail.com>
2023-01-14 13:02:30 +0600

    Random Pin Generator Done

--- JS/pin.js
+++ JS/pin.js
@@ -10,5 +10,18 @@ function getPin(){
 
 function generateRandomPin(){
     const random = Math.round(Math.random() * 10000);
+    console.log(random);
     return random;
-}
+};
+
+document.getElementById('pin-generate').addEventListener('click', function(){
+    const inputField = document.getElementById('show-pin');
+    inputField.value = getPin();
+    // console.log(inputField.value);
+});
+
+document.getElementById('copy').addEventListener('click', function () {
+    const copyText = document.getElementById('show-pin');
+    navigator.clipboard.writeText(copyText.value);
+    alert(`Pin Copied: ${copyText.value}`)
+});
